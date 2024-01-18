bbdc70285b97d913fdb93a9e2497319756e8b39a
TheRiisen <emilruff@gmail.com>
2024-01-18 16:54:24 +0100

    Diffusion
    diffusion implementation in models and addition to train to normalize input

--- a/models.py
+++ a/models.py
@@ -8,7 +8,7 @@ import numpy as np
 import pytorch_lightning as pl
 import torch
 import torch.distributions
-import torch.nn
+import torch.nn as nn
 import torch.optim
 import torchvision
 import torchvision.datasets
@@ -234,6 +234,274 @@ class VAEBeta(VAE):
         return loss
 
 
+
+
+class MyBlock(pl.LightningModule):
+    def __init__(self, shape, in_c, out_c, kernel_size=3, stride=1, padding=1, activation=None, normalize=True):
+        super(MyBlock, self).__init__()
+        self.ln = nn.LayerNorm(shape)
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size, stride, padding)
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size, stride, padding)
+        self.activation = nn.SiLU() if activation is None else activation
+        self.normalize = normalize
+
+    def forward(self, x):
+        out = self.ln(x) if self.normalize else x
+        out = self.conv1(out)
+        out = self.activation(out)
+        out = self.conv2(out)
+        out = self.activation(out)
+        return out
+    
+def sinusoidal_embedding(n, d):
+    # Returns the standard positional embedding
+    embedding = torch.zeros(n, d)
+    wk = torch.tensor([1 / 10_000 ** (2 * j / d) for j in range(d)])
+    wk = wk.reshape((1, d))
+    t = torch.arange(n).reshape((n, 1))
+    embedding[:,::2] = torch.sin(t * wk[:,::2])
+    embedding[:,1::2] = torch.cos(t * wk[:,::2])
+
+    return embedding
+
+def _make_te(self, dim_in, dim_out):
+  return nn.Sequential(
+    nn.Linear(dim_in, dim_out),
+    nn.SiLU(),
+    nn.Linear(dim_out, dim_out)
+  ) 
+
+class MyUNet(pl.LightningModule):
+    def __init__(self, n_steps=1000, time_emb_dim=100):
+        super(MyUNet, self).__init__()
+
+        # Sinusoidal embedding
+        self.time_embed = nn.Embedding(n_steps, time_emb_dim)
+        self.time_embed.weight.data = sinusoidal_embedding(n_steps, time_emb_dim)
+        self.time_embed.requires_grad_(False)
+
+        # First half
+        self.te1 = self._make_te(time_emb_dim, 1)
+        self.b1 = nn.Sequential(
+            MyBlock((1, 28, 28), 1, 10),
+            MyBlock((10, 28, 28), 10, 10),
+            MyBlock((10, 28, 28), 10, 10)
+        )
+        self.down1 = nn.Conv2d(10, 10, 4, 2, 1)
+
+        self.te2 = self._make_te(time_emb_dim, 10)
+        self.b2 = nn.Sequential(
+            MyBlock((10, 14, 14), 10, 20),
+            MyBlock((20, 14, 14), 20, 20),
+            MyBlock((20, 14, 14), 20, 20)
+        )
+        self.down2 = nn.Conv2d(20, 20, 4, 2, 1)
+
+        self.te3 = self._make_te(time_emb_dim, 20)
+        self.b3 = nn.Sequential(
+            MyBlock((20, 7, 7), 20, 40),
+            MyBlock((40, 7, 7), 40, 40),
+            MyBlock((40, 7, 7), 40, 40)
+        )
+        self.down3 = nn.Sequential(
+            nn.Conv2d(40, 40, 2, 1),
+            nn.SiLU(),
+            nn.Conv2d(40, 40, 4, 2, 1)
+        )
+
+        # Bottleneck
+        self.te_mid = self._make_te(time_emb_dim, 40)
+        self.b_mid = nn.Sequential(
+            MyBlock((40, 3, 3), 40, 20),
+            MyBlock((20, 3, 3), 20, 20),
+            MyBlock((20, 3, 3), 20, 40)
+        )
+
+        # Second half
+        self.up1 = nn.Sequential(
+            nn.ConvTranspose2d(40, 40, 4, 2, 1),
+            nn.SiLU(),
+            nn.ConvTranspose2d(40, 40, 2, 1)
+        )
+
+        self.te4 = self._make_te(time_emb_dim, 80)
+        self.b4 = nn.Sequential(
+            MyBlock((80, 7, 7), 80, 40),
+            MyBlock((40, 7, 7), 40, 20),
+            MyBlock((20, 7, 7), 20, 20)
+        )
+
+        self.up2 = nn.ConvTranspose2d(20, 20, 4, 2, 1)
+        self.te5 = self._make_te(time_emb_dim, 40)
+        self.b5 = nn.Sequential(
+            MyBlock((40, 14, 14), 40, 20),
+            MyBlock((20, 14, 14), 20, 10),
+            MyBlock((10, 14, 14), 10, 10)
+        )
+
+        self.up3 = nn.ConvTranspose2d(10, 10, 4, 2, 1)
+        self.te_out = self._make_te(time_emb_dim, 20)
+        self.b_out = nn.Sequential(
+            MyBlock((20, 28, 28), 20, 10),
+            MyBlock((10, 28, 28), 10, 10),
+            MyBlock((10, 28, 28), 10, 10, normalize=False)
+        )
+
+        self.conv_out = nn.Conv2d(10, 1, 3, 1, 1)
+
+    def forward(self, x, t):
+        # x is (N, 2, 28, 28) (image with positional embedding stacked on channel dimension)
+        t = self.time_embed(t)
+        n = len(x)
+        out1 = self.b1(x + self.te1(t).reshape(n, -1, 1, 1))  # (N, 10, 28, 28)
+        out2 = self.b2(self.down1(out1) + self.te2(t).reshape(n, -1, 1, 1))  # (N, 20, 14, 14)
+        out3 = self.b3(self.down2(out2) + self.te3(t).reshape(n, -1, 1, 1))  # (N, 40, 7, 7)
+
+        out_mid = self.b_mid(self.down3(out3) + self.te_mid(t).reshape(n, -1, 1, 1))  # (N, 40, 3, 3)
+
+        out4 = torch.cat((out3, self.up1(out_mid)), dim=1)  # (N, 80, 7, 7)
+        out4 = self.b4(out4 + self.te4(t).reshape(n, -1, 1, 1))  # (N, 20, 7, 7)
+
+        out5 = torch.cat((out2, self.up2(out4)), dim=1)  # (N, 40, 14, 14)
+        out5 = self.b5(out5 + self.te5(t).reshape(n, -1, 1, 1))  # (N, 10, 14, 14)
+
+        out = torch.cat((out1, self.up3(out5)), dim=1)  # (N, 20, 28, 28)
+        out = self.b_out(out + self.te_out(t).reshape(n, -1, 1, 1))  # (N, 1, 28, 28)
+
+        out = self.conv_out(out)
+
+        return out
+
+    def _make_te(self, dim_in, dim_out):
+        return nn.Sequential(
+            nn.Linear(dim_in, dim_out),
+            nn.SiLU(),
+            nn.Linear(dim_out, dim_out)
+        )
+
+class MyDDPM(pl.LightningModule):
+    def __init__(self, network=MyUNet(1000), n_steps=1000, min_beta=10 ** -4, max_beta=0.02, device=None, image_chw=(1, 28, 28)):
+        super(MyDDPM, self).__init__()
+        self.n_steps = n_steps
+        #self.device = device
+        self.T = n_steps
+        self.image_chw = image_chw
+        self.network = network.to(self.device)
+        self.betas = torch.linspace(min_beta, max_beta, n_steps).to(
+            device)  # Number of steps is typically in the order of thousands
+        self.alphas = 1 - self.betas
+        self.alpha_bars = torch.tensor([torch.prod(self.alphas[:i + 1]) for i in range(len(self.alphas))]).to(self.device)
+        self._image_dir = None
+
+    @property
+    def image_dir(self):
+        if self._image_dir is None:
+            self._image_dir = os.path.join(self.logger.log_dir, "figures")
+            os.makedirs(self._image_dir, exist_ok=True)
+
+        return self._image_dir
+
+
+    def add_noise(self, x0, t, eta=None):
+        # Make input image more noisy (we can directly skip to the desired step)
+        n, c, h, w = x0.shape
+        a_bar = self.alpha_bars[t]
+
+        if eta is None:
+            eta = torch.randn(n, c, h, w).to(self.device)
+
+        noisy = a_bar.sqrt().reshape(n, 1, 1, 1) * x0 + (1 - a_bar).sqrt().reshape(n, 1, 1, 1) * eta
+        return noisy
+
+    def forward(self, x, t): #backward
+        # Run each image through the network for each timestep t in the vector t.
+        # The network returns its estimation of the noise that was added.
+        return self.network(x, t)
+    
+    def training_step(self, batch, _):
+        #print("training_step")
+        xs, _ = batch
+        ts = torch.from_numpy(np.random.choice(self.T, xs.shape[0])).to(self.device)
+        epsilons = torch.normal(0, 1, xs.shape, device=self.device)
+
+        alpha_bar_ts = torch.tensor(
+            [torch.prod(1 - self.betas[:t]) for t in ts],
+            device=self.device
+        ).reshape(-1, 1, 1, 1).repeat(1, 1, 28, 28)
+        
+        xts = torch.sqrt(alpha_bar_ts) * xs + torch.sqrt(1 - alpha_bar_ts) * epsilons
+        
+        error = ((epsilons - self(xts, ts))**2).sum(axis=-1).sum(-1).sum(-1).mean()
+        self.log('error', error)
+
+        return error
+
+
+    def mu(self, xt, t):
+        factor = 1 / torch.sqrt(1 - self.betas[t[0]])
+        alpha_bar_t = torch.prod(1 - self.betas[:t[0]])
+        #print(t) val
+        #print(factor.shape) empty
+        #print(alpha_bar_t.shape) empty
+        # print("factor",factor.shape)
+        # print("alpha_bar_t",alpha_bar_t.shape)
+        # print("t",t)
+        # print("xt",xt.shape)
+        return factor * (xt - self.betas[t[0]] / torch.sqrt(1 - alpha_bar_t) * self(xt, t))
+    
+    def generate_samples(self, n):
+        with torch.no_grad():
+            xs = torch.normal(0, 1, size=(n, 1, 28, 28), dtype=torch.float32, device=self.device)
+
+            for t in range(self.T, 1, -1):
+                xs = torch.normal(self.mu(xs, torch.tensor([t - 1]).repeat(n,)), torch.sqrt(self.betas[-1]).to(self.device))
+
+        return xs
+    
+    def validation_step(self, batch, batch_index):
+        #print("val_check")
+        if (batch_index == 0):#self.trainer.current_epoch % 10 == 0 and batch_index == 0:
+            xs, _ = batch
+            xs = xs[0].unsqueeze(0)
+            #print(xs.shape)
+            ts = torch.tensor([0, 5, 10, 20, 40, 100, 250, 500, 800]).to(self.device)
+            epsilons = torch.normal(0, 1, xs.shape, device=self.device)
+
+            alpha_bar_ts = torch.tensor(
+                [torch.prod(1 - self.betas[:t]) for t in ts],
+                device=self.device
+            ).reshape(-1, 1, 1, 1).repeat(1, 1, 28, 28)
+            
+            xts = torch.sqrt(alpha_bar_ts) * xs + torch.sqrt(1 - alpha_bar_ts) * epsilons
+            torchvision.utils.save_image(
+                xts.cpu(),
+                os.path.join(
+                    self.image_dir, f"noises_{self.trainer.current_epoch}.png"
+                ),
+                nrow=3,
+            )
+
+            #print("generate")
+            samples = self.generate_samples(16)
+            #print("geenrate done")
+            torchvision.utils.save_image(
+                samples.cpu(),
+                os.path.join(
+                    self.image_dir, f"samples_{self.trainer.current_epoch}.png"
+                ),
+                nrow=4,
+            )
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-3)
+
+
+
+
+
+
+
+
 class Diffusion(pl.LightningModule):
     def __init__(self, T: int) -> None:
         super().__init__()
@@ -281,6 +549,7 @@ class Diffusion(pl.LightningModule):
         zs = xs + self.time(ts_embedding).reshape(-1, 1, 28, 28)
         zs = self.net(zs)
 
+        print(zs.shape)
         return zs
 
     def training_step(self, batch, _):
@@ -303,7 +572,13 @@ class Diffusion(pl.LightningModule):
     def mu(self, xt, t):
         factor = 1 / torch.sqrt(1 - self.betas[t])
         alpha_bar_t = torch.prod(1 - self.betas[:t])
-
+        #print(t) val
+        #print(factor.shape) empty
+        #print(alpha_bar_t.shape) empty
+        print("factor",factor.shape)
+        print("alpha_bar_t",alpha_bar_t.shape)
+        print("t",t)
+        print("xt",xt.shape)
         return factor * (xt - self.betas[t] / torch.sqrt(1 - alpha_bar_t) * self(xt, torch.tensor([t])))
 
     def generate_samples(self, n):
